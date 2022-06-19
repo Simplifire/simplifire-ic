83f1f7f87098550218546bb2350f8b9140e6c596
Marcin Biegala <marcin@codeclusive.io>
2022-06-19 15:06:58 +0200

    Split backend functions into separate files for clarity

---
+++ src/rust_simplifire/document.rs
@@ -0,0 +1,105 @@
+
+use ic_cdk_macros::*;
+
+use crate::{RUNTIME_STATE, RuntimeState, Document, DocumentVersion};
+
+#[update]
+fn add_doc(current_editor_id: u32, name: String) -> u32 {
+    RUNTIME_STATE.with(|state| add_doc_impl(current_editor_id, name, &mut state.borrow_mut()))
+}
+fn add_doc_impl(current_editor_id: u32, name: String, runtime_state: &mut RuntimeState) -> u32 {
+    let id = runtime_state.env.random_u32();
+    let now = runtime_state.env.now();
+
+    runtime_state.data.documents.push(Document {
+        id,
+        current_editor_id,
+        name,
+        active: true,
+        added: now,
+        updated: now,
+        deleted: 0,
+    });
+
+    id
+}
+
+#[query]
+fn get_docs(active_filter: Option<bool>) -> Vec<Document> {
+    RUNTIME_STATE.with(|state| get_docs_impl(active_filter, &state.borrow()))
+}
+fn get_docs_impl(active_filter: Option<bool>, runtime_state: &RuntimeState) -> Vec<Document> {
+    runtime_state.data.documents.iter().filter(|i| active_filter.map_or(true, |d| i.active == d)).cloned().collect()
+}
+
+#[query]
+fn get_doc(doc_id: u32) -> Option<Document> {
+    RUNTIME_STATE.with(|state| get_doc_impl(doc_id, &state.borrow()))
+}
+fn get_doc_impl(doc_id: u32, runtime_state: &RuntimeState) -> Option<Document> {
+    runtime_state.data.documents.iter().filter(|i| i.id == doc_id).cloned().next()
+}
+
+#[update]
+fn update_doc(id: u32, current_editor_id: u32, name: String) -> bool {
+    RUNTIME_STATE.with(|state| update_doc_impl(id, current_editor_id, name, &mut state.borrow_mut()))
+}
+fn update_doc_impl(id: u32, current_editor_id: u32, name: String, runtime_state: &mut RuntimeState) -> bool {
+    if let Some(document) = runtime_state.data.documents.iter_mut().find(|i| i.id == id) {
+        document.active = true;
+        document.current_editor_id = current_editor_id;
+        document.name = name;
+        true
+    } else {
+        false
+    }
+}
+
+
+#[update]
+fn add_document_version(
+        document_id: u32, version_number: u32, editor_user_id: u32, content: String)
+         -> u32 {
+    RUNTIME_STATE.with(|state| add_document_version_impl(
+        document_id, version_number, editor_user_id, content, &mut state.borrow_mut()))
+}
+fn add_document_version_impl(document_id: u32, version_number: u32, editor_user_id: u32, content: String, runtime_state: &mut RuntimeState) -> u32 {
+    let id = runtime_state.env.random_u32();
+    let now = runtime_state.env.now();
+
+    runtime_state.data.documentversions.push(DocumentVersion {
+        id,
+        document_id,
+        version_number,
+        editor_user_id,
+        content,
+        active: true,
+        added: now,
+        updated: now,
+        deleted: 0,
+    });
+
+    id
+}
+
+#[query]
+fn get_document_versions(active_filter: Option<bool>) -> Vec<DocumentVersion> {
+    RUNTIME_STATE.with(|state| get_document_versions_impl(active_filter, &state.borrow()))
+}
+
+fn get_document_versions_impl(active_filter: Option<bool>, runtime_state: &RuntimeState) -> Vec<DocumentVersion> {
+    runtime_state.data.documentversions.iter().filter(|i| active_filter.map_or(true, |d| i.active == d)).cloned().collect()
+}
+
+#[update]
+fn update_document_version(id: u32, content: String) -> bool {
+    RUNTIME_STATE.with(|state| update_document_version_impl(id, content, &mut state.borrow_mut()))
+}
+fn update_document_version_impl(id: u32, content: String, runtime_state: &mut RuntimeState) -> bool {
+    if let Some(document_version) = runtime_state.data.documentversions.iter_mut().find(|i| i.id == id) {
+        document_version.content = content;
+        true
+    } else {
+        false
+    }
+}
---
+++ src/rust_simplifire/user.rs
@@ -0,0 +1,51 @@
+
+use ic_cdk_macros::*;
+
+use crate::{RUNTIME_STATE, User, RuntimeState};
+
+#[update]
+fn add_user(principal_id: u32, first_name: String, last_name: String, email: String) -> u32 {
+    RUNTIME_STATE.with(|state| add_user_impl(principal_id, first_name, last_name, email, &mut state.borrow_mut()))
+}
+fn add_user_impl(principal_id: u32, first_name: String, last_name: String, email: String, runtime_state: &mut RuntimeState) -> u32 {
+    let id = runtime_state.env.random_u32();
+    let now = runtime_state.env.now();
+
+    runtime_state.data.users.push(User {
+        id,
+        principal_id,
+        first_name,
+        last_name,
+        email,
+        active: true,
+        added: now,
+        updated: now,
+        deleted: 0,
+    });
+
+    id
+}
+
+#[query]
+fn get_users(active_filter: Option<bool>) -> Vec<User> {
+    RUNTIME_STATE.with(|state| get_users_impl(active_filter, &state.borrow()))
+}
+
+fn get_users_impl(active_filter: Option<bool>, runtime_state: &RuntimeState) -> Vec<User> {
+    runtime_state.data.users.iter().filter(|i| active_filter.map_or(true, |d| i.active == d)).cloned().collect()
+}
+
+#[update]
+fn update_user(id: u32, first_name: String, second_name: String, email: String) -> bool {
+    RUNTIME_STATE.with(|state| update_user_impl(id, first_name, second_name, email, &mut state.borrow_mut()))
+}
+fn update_user_impl(id: u32, first_name: String, second_name: String, email: String, runtime_state: &mut RuntimeState) -> bool {
+    if let Some(user) = runtime_state.data.users.iter_mut().find(|i| i.id == id) {
+        user.first_name = first_name;
+        user.last_name = second_name;
+        user.email = email;
+        true
+    } else {
+        false
+    }
+}
